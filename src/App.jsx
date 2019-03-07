147a50172ae083bc96061704741b8b0ee68cd9f3
Guest User <taylorgreatlakes@yahoo.com>
2019-03-06 16:22:33 -0800

    changed stuff

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      food:""
+
+    }
+  }
   render() {
     return (
       <div className="App">
        <h1>Get me Food!</h1>
        <br></br>
-       <p>(a neat tool designed to help users find the best rated restaurants around them)</p>
+       <p>(a neat tool designed to help users find the best rated restaurants around them.)</p>
+       <form>
+
+      </form>
       </div>
     );
   }
