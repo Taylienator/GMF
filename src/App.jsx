33fe9828d5b8818a11b13ccee5f8817754cebb71
Guest User <taylorgreatlakes@yahoo.com>
2019-03-06 16:29:17 -0800

    added in some state

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,15 @@ class App extends Component {
       food:""
 
     }
+    this.handleInput = this.handleInput.bind(this)
+  }
+
+  handleInput(e){
+    let yumFood = e.target.value;
+    this.setState({
+      food: yumFood
+    })
+    console.log(this.state.food)
   }
   render() {
     return (
@@ -16,8 +25,8 @@ class App extends Component {
        <h1>Get me Food!</h1>
        <br></br>
        <p>(a neat tool designed to help users find the best rated restaurants around them.)</p>
-       <form>
-
+       <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="What do you want to eat?" onChange={this.handleInput} value={this.state.food}/>
       </form>
       </div>
     );
