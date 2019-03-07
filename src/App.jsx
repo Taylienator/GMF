899105006b54f18893dc17c2bc842da51cac3653
Guest User <taylorlake@Taylors-MacBook-Pro.local>
2019-03-06 16:15:49 -0800

    stuff

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Get me Food!</h1>
+       <br></br>
+       <p>(a neat tool designed to help users find the best rated restaurants around them)</p>
+      </div>
+    );
+  }
+}
+
+export default App;
