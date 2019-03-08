8479364d317ff15c5b6924b2621c95c6d87c9e3d
Guest User <taylorgreatlakes@yahoo.com>
2019-03-07 17:20:25 -0800

    updated state to manipulate render

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FoodChoice from './FoodChoice';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      food:""
+      foodinput:"",
+      chosenfood:"",
+      list: []
 
     }
     this.handleInput = this.handleInput.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    
   }
 
   handleInput(e){
-    let yumFood = e.target.value;
-    this.setState({
-      food: yumFood
+   this.setState({
+      foodinput: e.target.value
     })
-    console.log(this.state.food)
   }
+  
+  handleSubmit(e){
+    e.preventDefault()
+    let arrOf = this.state.food
+    this.state.list.push(arrOf)
+    this.setState({
+      chosenfood:this.state.foodinput
+    })
+    console.log(this.state.chosenfood)
+    console.log(this.state.list.length, "length")
+    }
   render() {
     return (
       <div className="App">
@@ -27,8 +40,14 @@ class App extends Component {
        <p>(a neat tool designed to help users find the best rated restaurants around them.)</p>
        <form onSubmit={this.handleSubmit}>
         <input type="text" placeholder="What do you want to eat?" onChange={this.handleInput} value={this.state.food}/>
+        <input type="submit" value="Submit" />
       </form>
+ 
+      { this.state.chosenfood && 
+      <FoodChoice foodchoice = {this.state.chosenfood}/> 
+     }
       </div>
+      
     );
   }
 }
